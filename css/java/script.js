c79a7f2b3830fe59470e990155abecf67a79fcb6
AnnyGaby180 <annygaby1803@gmal.com>
2024-10-23 22:05:25 -0300

    Site mda mor

--- css/java/script.js
+++ css/java/script.js
@@ -7,3 +7,22 @@ const productGrid = document.getElementById('product-grid');
 moreProductsButton.addEventListener('click', () => {
     productGrid.style.display = 'grid'; // Mostra os produtos ao clicar no botão
 });
+
+
+
+function moveSlide(button, direction) {
+    const carousel = button.parentElement;
+    const images = carousel.querySelectorAll('.carousel-image');
+    let currentIndex = Array.from(images).findIndex(img => img.classList.contains('active'));
+
+    // Remove a classe 'active' da imagem atual
+    images[currentIndex].classList.remove('active');
+
+    // Calcula o novo índice
+    currentIndex += direction;
+    if (currentIndex < 0) currentIndex = images.length - 1; // Volta para a última imagem se estiver no início
+    if (currentIndex >= images.length) currentIndex = 0; // Volta para a primeira imagem se estiver no final
+
+    // Adiciona a classe 'active' na nova imagem
+    images[currentIndex].classList.add('active');
+}
